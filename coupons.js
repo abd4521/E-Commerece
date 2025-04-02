683e7334a4890c87a244544e9b77dea16bf36d61
abd4521 <aiuser4521@gmail.com>
2025-04-03 00:33:55 +0500

    Add files via upload

---
+++ coupons.js
@@ -0,0 +1,5 @@
+const validCoupons = {
+    "TRENDS10": 0.1,  // 10% off
+    "WELCOME20": 0.2, // 20% off
+    "SPECIAL15": 0.15 // 15% off
+  };
---
+++ products.js
@@ -0,0 +1,58 @@
+const products = [
+    {
+      id: "1",
+      name: "Ultra 9 Smartwatch",
+      price: 1000,
+      image: "images.jpeg",
+      stock: 10,
+      category: "smartwatch"
+    },
+    {
+      id: "2",
+      name: "Ultra 10 Smartwatch",
+      price: 1500,
+      image: "download.jpeg",
+      stock: 5,
+      category: "smartwatch"
+    },
+    {
+      id: "3",
+      name: "Garmin Vívoactive 5",
+      price: 5500,
+      image: "download1.jpeg",
+      stock: 8,
+      category: "fitness"
+    },
+    {
+      id: "4",
+      name: "Fitbit Versa 4",
+      price: 4500,
+      image: "download (2).jpeg",
+      stock: 3,
+      category: "fitness"
+    },
+    {
+      id: "5",
+      name: "M3 Ultra Smartwatch",
+      price: 9500,
+      image: "download (3).jpeg",
+      stock: 7,
+      category: "smartwatch"
+    },
+    {
+      id: "6",
+      name: "TicWatch Pro 5",
+      price: 7500,
+      image: "images ().jpeg",
+      stock: 4,
+      category: "smartwatch"
+    },
+    {
+      id: "7",
+      name: "Fitbit Charge 6",
+      price: 3500,
+      image: "download (5).jpeg",
+      stock: 12,
+      category: "fitness"
+    }
+  ];
